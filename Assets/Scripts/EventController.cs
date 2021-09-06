386bfc8d4dac7d0f28e6bfe12a0b5c2f754782d8
Lizzie264 <57414831+Lizzie264@users.noreply.github.com>
2021-09-06 15:22:45 -0500

    Saimon spawn controller

---
+++ Assets/Scripts/EventController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject[] events;
+    public GameObject distance;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
